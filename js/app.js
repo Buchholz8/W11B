dcf1727e81bbda36dee021222c9b3b9928e59a83
markb <markbuchholz8@gmail.com>
2023-04-01 07:22:10 -0600

    updates and changes to all files

--- js/app.js
+++ js/app.js
@@ -1,15 +1,37 @@
-let selection_json = Cookies.get(`slection`)
-options = {
+let types = [
+    {
+        shirts: `T-shirt`,
+        description: `These are shirts`,
+        price: 20,
+    },
+    {
+        Hoodies: `hoodies`,
+        description: `These are hoodies`,
+        price: 80,
+    },
+    {
+        pants: `jeans`,
+        description: `These are jeans`,
+        price: 60,
+    },
+    {
+        hats: `sports hats`,
+        description: `These are hats`,
+        price: 40,
+    },
+]
+let selection_json = Cookies.get(`slection`);
+let options = {
     Shirts: 0,
     Hoodies: 0,
     Pants:0,
     Hats:0,
 }
 if(!(selection_json === undefined)){
-    options = JSON.parse(`selection`)
+    options = JSON.parse(selection_json);
 }
 function add_options(details){
-    options[details[`target`].getAttribute(`key_name`)] +=1;
+    options[details[`target`].getAttribute(`key_name`)] += 1;
     selection_json = JSON.stringify(options)
     Cookies.set(`selection` , selection_json)
 }
--- js/checkout.js
+++ js/checkout.js
@@ -1,11 +1,9 @@
-let selection_json = Cookies.get(`slection`)
-let Choice_container = document.querySelector(`#Choice_div`)
+
+let selection_json = Cookies.get(`slection`);
+let Choice_container = document.querySelector(`#Choice_div`);
 if(selection_json === undefined){
-    Choice_container.insertAdjacentHTML(`afterbegin` , `<h2> You have not made a choice yet</h2>`)
+    Choice_container.insertAdjacentHTML(`afterbegin` , `<h2> You have not made a choice yet</h2>`);
 } else {
-    decision = JSON.parse(selection_json)
-    Choice_container.insertAdjacentHTML(`afterbegin` ,  `<h2> Shirts: ${decision[`Shirts`]}</h2>` )
-    Choice_container.insertAdjacentHTML(`afterbegin` ,  `<h2> Shirts: ${decision[`Hoodies`]}</h2>` )
-    Choice_container.insertAdjacentHTML(`afterbegin` ,  `<h2> Shirts: ${decision[`Pants`]}</h2>` )
-    Choice_container.insertAdjacentHTML(`afterbegin` , `<h2> Shirts: ${decision[`Hats`]}</h2>` )
-}
+    slected_item = JSON.parse(selection_json);
+    Choice_container.insertAdjacentHTML(`afterbegin` ,`<h2> You chose ${slected_item[i][key_name]} ! </h2>`)
+};
