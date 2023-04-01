29fb6d8f5625aaef0dd60846299622403a011085
markb <markbuchholz8@gmail.com>
2023-04-01 08:25:21 -0600

    final //

--- js/checkout.js
+++ js/checkout.js
@@ -5,17 +5,19 @@ let products_json = Cookies.get(`products_cook`)
 //I define a place for the new info to go
 let Choice_container = document.querySelector(`#Choice_div`);
 
-//everything seems to work up to this point, it all seems to be undefined even when i click a button
+//everything seems to work up to this point, it all seems to be undefined even when i click a button its probably due to the key_name but i couldnt figure it out                 
 
 //I add an if loop that will determine if the cookie is defined or not
 if(selection_json === undefined){
     Choice_container.insertAdjacentHTML(`afterbegin` , `<h2> You have not made a choice yet</h2>`);
-} else {
+}
+//an else statement that will add the new information based off the saved cookies and their keynames
+else {
     slected_item = JSON.parse(selection_json);
     products_info = JSON.parse(products_json)
     Choice_container.insertAdjacentHTML(`afterbegin` ,`<h2> You chose ${slected_item[`key_name`]} ${products_info[`key_name`]} ! </h2>`)
 };
 
-//an else statement that will add the new information based off the saved cookie and the keyname 
+
 
 
